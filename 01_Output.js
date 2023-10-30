4d2e30264995f1130e236af72b0e13fdcee29c33
eodaneze <ezealiokechukwu1999@gmail.com>
2023-10-30 06:44:16 -0700

    js display

---
+++ 01_Output.js
@@ -0,0 +1,9 @@
+// console
+console.log("I am learning how to code");
+
+// inner html
+document.getElementById('demo').innerHTML = "This is coming from js";
+// document.write
+document.write("This is only used for testing purpose");
+// alert
+window.alert("This is coming from the alert box")
