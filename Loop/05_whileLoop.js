41e87dd9ff26aabf08230be228184a36a1446703
eodaneze <ezealiokechukwu1999@gmail.com>
2023-11-01 06:08:17 -0700

    js while loop

---
+++ Loop/05_whileLoop.js
@@ -0,0 +1,17 @@
+// The while loop loops through a block of code as long as a specified condition is true.
+
+let i = 0;
+let text = ""
+while (i < 10) {
+    console.log(`the number is ${i}`) + '<br>';
+    i++;
+}
+/*  
+ The do while loop is a variant of the while loop. This loop will execute the code block once, before checking if the condition is true, then it will repeat the loop as long as the condition is true.
+*/
+
+let x = 0;
+do {
+    console.log(`the number is ${x}`)  + '<br>';
+    x++;
+} while (x < 10);
