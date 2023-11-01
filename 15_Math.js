ca69f1a1d55d5e06fa65ef09ea3b7441cfc65e96
eodaneze <ezealiokechukwu1999@gmail.com>
2023-11-01 04:06:59 -0700

    js math

---
+++ 15_Math.js
@@ -0,0 +1,12 @@
+console.log(Math.PI);
+const num = 23.567
+console.log(Math.round(num)) //	Returns x rounded to its nearest integer;
+console.log(Math.ceil(num)) //	Returns x rounded up to its nearest integer;
+console.log(Math.floor(num)) //Returns x rounded down to its nearest integer;
+console.log(Math.trunc(num)) //	Returns the integer part of x;
+console.log(Math.pow(2,3));
+console.log(Math.sqrt(64));
+console.log(Math.abs(-4));
+console.log(Math.min(2,3,4,8,9));
+console.log(Math.max(2,3,4,8,9));
+console.log(Math.random());
